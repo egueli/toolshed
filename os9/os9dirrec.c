9a2066125f418e26eff98dd612505deca1a47142
Enrico Gueli <enrico.gueli@gmail.com>
2023-08-03 09:43:31 +0200

    remove dead code & vars

--- os9/os9dirrec.c
+++ os9/os9dirrec.c
@@ -12,15 +12,9 @@
 static char *strcatdup( char *orig, char *cat1, char *cat2 );
 static error_code ParseFDSegList(fd_stats *fd, u_int dd_tot, char *path, unsigned char *secondaryBitmap );
 static error_code ProcessDirectorySector(os9_path_id os9_path, u_int fd_siz, u_int dd_tot, int dir_lsn, char *path, u_int *count);
-static error_code BuildSecondaryAllocationMap( os9_path_id os9_path, int dir_lsn, char *path, unsigned char *secondaryBitmap );
-static error_code CompareAllocationMap( unsigned char *primaryAlloMap, unsigned char *secondaryBitmap, int dd_map, int cluster_size );
-static void PathlistsForQuestionableClusters();
-static void FreeQuestionableMemory();
 static void AddQuestionableCluster( int cluster );
 static void AddPathToBit( int lsn, char *path );
 static int do_dirrec(char **argv, char *p, int lsn);
-static void PathlistsForQuestionableClusters(void);
-static void FreeQuestionableMemory(void);
 
 
 /* Help message */
@@ -38,8 +32,6 @@ static int	gFolderCount,
 	gFnotA,			/* Incremented when cluster is used and not in allocattion map			*/
 	gAnotF,			/* Incremented when an allocated bit is not in the file structure		*/
 	gBadFD; 		/* Incremented when a file descriptor has an out of range file segment  */
-
-static int	sOption, bOption, pOption;	/* Flags for command line options */
 
 typedef struct qCluster_t
 {
@@ -275,119 +267,6 @@ static error_code ProcessDirectorySector(os9_path_id os9_path, u_int fd_siz, u_i
 	return ec;
 }
 
-/* This function will drill down into a directory file and fillout a secondary allocation bitmap.
-   It is recursive, so whenever a directory is encoundered it will call itself. It will also compare
-   the map it creates with the map suppilied that represents the map on disk. */
-   
-static error_code BuildSecondaryAllocationMap( os9_path_id os9_path, int dir_lsn, char *path, unsigned char *secondaryBitmap )
-{
-	error_code 	ec = 0;
-	fd_stats	*dir_fd;
-	u_int		dd_tot,
-				fd_siz,
-				count,
-				i, j;
-	Fd_seg		theSeg;
-	int			bps = os9_path->bps;
-
-	/* Check if this directory has already been drilled into */
-
-	if ( _os9_ckbit( secondaryBitmap, dir_lsn ) != 0)
-	{
-		/* Whoops, it is already allocated! */
-		printf("Directory %s has a circular reference. Skipping\n", path);
-		AddQuestionableCluster(dir_lsn);
-		return(0);
-	}
-	
-	/* Allocate directory file descriptor LSN in secondary allocation map */
-	
-	_os9_allbit(secondaryBitmap, dir_lsn, 1);
-	gFolderCount++;
-	
-	dd_tot = int3(os9_path->lsn0->dd_tot);
-	
-	dir_fd = (fd_stats *)malloc( bps );
-	if( dir_fd == NULL )
-	{
-		printf("Out of memory, terminating (001).\n");
-		exit(-1);
-	}
-	
-	if (read_lsn(os9_path, dir_lsn, dir_fd ) != bps)
-	{
-		printf("Sector wrong size, terminating (001).\n" );
-		printf("LSN: %d\n", dir_lsn );
-		exit(-1);
-	}
-
-	/* Parse segment list of directory, report any problems */
-	ec = ParseFDSegList( dir_fd, dd_tot, path, secondaryBitmap );
-	
-	if (ec != 0)
-	{
-		printf("File descriptor for directory %s is bad. Will not open directory file.\n", path);
-		ec = 0;
-	}
-	else
-	{
-		/* Now open directory file and parse contents */
-		fd_siz = int4(dir_fd->fd_siz);
-		count = 0;
-		i = 0;
-		
-		while (int3(dir_fd->fd_seg[i].lsn) != 0)
-		{
-			theSeg = &(dir_fd->fd_seg[i]);
-			
-			if (i > NUM_SEGS)
-			{
-				break;
-			}
-				
-			if (count > fd_siz)
-			{
-				break;
-			}
-			
-			if (int2(theSeg->num) > dd_tot)
-			{
-				printf("File: %s contains a bad segment (%d > %d)\n", path, int2(theSeg->num), dd_tot );
-				gBadFD++;
-				break;
-			}
-			
-			for (j = 0; j < int2(theSeg->num); j++ )
-			{
-				if (count > fd_siz)
-					break;
-				
-				if (int3(theSeg->lsn) + j > dd_tot)
-				{
-					printf("File: %s, contains bad LSN (%d > %d)\n", path, int3(theSeg->lsn)+j, dd_tot);
-					count += 256;
-					gBadFD++;
-					break;
-				}
-				
-				int dir_lsn = int3(theSeg->lsn)+j;
-
-				ec = ProcessDirectorySector(os9_path, fd_siz, dd_tot, dir_lsn, path, &count);			
-				if (ec != 0)
-				{
-					return ec;
-				}
-			}
-			
-			i++;
-		}
-	}
-	
-	free(dir_fd);
-	
-	return(ec);
-}
-
 static error_code ParseFDSegList( fd_stats *fd, u_int dd_tot, char *path, unsigned char *secondaryBitmap )
 {
 	error_code	ec = 0;
@@ -455,100 +334,6 @@ static error_code ParseFDSegList( fd_stats *fd, u_int dd_tot, char *path, unsign
 	}
 	
 	return ec;
-}
-
-static error_code CompareAllocationMap( unsigned char *primaryAlloMap, unsigned char *secondaryBitmap, int dd_map, int cluster_size )
-{
-	error_code ec = 0;
-	int i, j, LSN;
-	
-	for(i=0; i< (dd_map / cluster_size); i++ )
-	{
-		int p, s;
-		
-		p = _os9_ckbit( primaryAlloMap, i );
-		
-		for( j=0; j<cluster_size; j++ )
-		{
-			LSN = i*cluster_size+j;
-			
-			s = _os9_ckbit( secondaryBitmap, LSN );
-			
-			if( p != s )
-			{
-				if( p == 0 )
-				{
-					printf("Logical sector %d ($%6.6X) of cluster %d ($%6.6X) in file structure but not in allocation map\n", LSN, LSN, i, i );
-					AddQuestionableCluster( i );
-					gFnotA++;
-				}
-				
-				if( s == 0 )
-				{
-					if( bOption == 0 )
-						printf("Logical sector %d ($%6.6X) of cluster %d ($%6.6X) in allocation map but not in file structure\n", LSN, LSN, i, i );
-						
-					gAnotF++;
-				}
-			}
-		}
-	}
-
-	return(ec);
-}
-
-static void PathlistsForQuestionableClusters(void)
-{
-	qCluster_t	*tmp;
-	qBitPath_t	*bitpath, *tmpBP;
-	
-	while( qCluster != NULL )
-	{
-		bitpath = gBitPaths;
-		
-		while( bitpath != NULL )
-		{
-			if( qCluster->lsn == bitpath->lsn )
-				printf("Cluster $%6.6X in path: %s\n", qCluster->lsn, bitpath->path );
-			
-			bitpath = bitpath->next;
-		}
-
-		tmp = qCluster->next;
-		free( qCluster );
-		qCluster = tmp;
-	}
-	
-	bitpath = gBitPaths;
-		
-	while( bitpath != NULL )
-	{
-		free( bitpath->path );
-		tmpBP = bitpath->next;
-		free( bitpath );
-		bitpath = tmpBP;
-	}
-}
-
-static void FreeQuestionableMemory(void)
-{
-	qCluster_t	*tmp;
-	qBitPath_t	*tmpBP;
-	
-	while( qCluster != NULL )
-	{
-		tmp = qCluster->next;
-		free( qCluster );
-		qCluster = tmp;
-	}
-
-	while( gBitPaths != NULL )
-	{
-		free( gBitPaths->path );
-		tmpBP = gBitPaths->next;
-		free( gBitPaths );
-		gBitPaths = tmpBP;
-	}
 }
 
 static char *strcatdup( char *orig, char *cat1, char *cat2 )
