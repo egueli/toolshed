48896822b2bafba939e52d1c05fe4e3a15fbd614
Enrico Gueli <enrico.gueli@gmail.com>
2024-03-02 22:31:48 +0100

    use allocation map to scan thru clusters

--- os9/os9fscan.c
+++ os9/os9fscan.c
@@ -11,7 +11,8 @@
 #include <math.h>
 
 static int do_fscan(char **argv, char *p);
-static error_code ProcessFileDescriptor(os9_path_id os9_path, u_int *next_lsn, char *path);
+static error_code ProcessFileDescriptor(os9_path_id os9_path, u_int next_lsn, char *path, unsigned char* secondaryBitmap);
+static error_code MarkFileSectors(os9_path_id os9_path, fd_stats *fd, unsigned char* secondaryBitmap, u_int lsn);
 
 /* Help message */
 static char const * const helpMessage[] =
@@ -121,18 +122,35 @@ static int do_fscan(char **argv, char *p)
 	
 	u_int	dd_tot = int3(os9_path->lsn0->dd_tot);
 
-	u_int lsn = int3(os9_path->lsn0->dd_dir);
-	while (lsn < dd_tot)
+	unsigned char  *secondaryBitmap = (unsigned char *)malloc( (dd_tot+1) / 8 );
+	if( secondaryBitmap == NULL )
 	{
-		u_int next_lsn = lsn;
-		u_int result = ProcessFileDescriptor(os9_path, &next_lsn, p);
-		if (result == 0) {
-			printf("0x%x\n", lsn);
-			lsn = next_lsn;
-		} else {
-			lsn++;
+		printf("Failed to allocate memory for the secondary bitmap.\n");
+		return -1;
+	}
+	memset(secondaryBitmap, 0, (dd_tot + 1) / 8);
+
+	/* Everything before the root directory is surely not a file, so
+	   mark it as already scanned */
+	_os9_allbit(secondaryBitmap, 0, int3(os9_path->lsn0->dd_dir));
+
+	while (1)
+	{
+		// Note: _os9_getfreebit allocates the next free bit and returns its index.
+		int free_lsn = _os9_getfreebit(secondaryBitmap, dd_tot);
+		if (free_lsn == -1) {
+			break;
 		}
-		break; // TODO use allocation bitmap to find the next LSN to scan
+		// printf("processing: 0x%0x\n", free_lsn);
+		u_int result = ProcessFileDescriptor(os9_path, (u_int)free_lsn, p, secondaryBitmap);
+		if (result == 0) {
+			printf("0x%x\n", free_lsn);
+		}
+		else {
+			// printf("not an FD: 0x%x\n", free_lsn);
+		}
+		// Mark all the other sectors of this cluster as scanned.
+		_os9_allbit(secondaryBitmap, free_lsn, cluster_size);
 	}
 	
 	_os9_close(os9_path);
@@ -140,16 +158,16 @@ static int do_fscan(char **argv, char *p)
     return ec;
 }
 
-static error_code ProcessFileDescriptor(os9_path_id os9_path, u_int *lsn, char *path)
+static error_code ProcessFileDescriptor(os9_path_id os9_path, u_int lsn, char *path, unsigned char *secondaryBitmap)
 {
 	error_code 	    ec = 0;
 	int			    bps = os9_path->bps;
 	u_int			dd_tot = int3(os9_path->lsn0->dd_tot);
 	fd_stats		fd;
 
-	if (read_lsn(os9_path, *lsn, &fd) != bps)
+	if (read_lsn(os9_path, lsn, &fd) != bps)
 	{
-		printf("Sector wrong size, terminating (002).\nLSN: %d\n", *lsn );
+		printf("Sector wrong size, terminating (002).\nLSN: 0x%06x\n", lsn );
 		exit(-1);
 	}
 
@@ -159,19 +177,21 @@ static error_code ProcessFileDescriptor(os9_path_id os9_path, u_int *lsn, char *
 		return ec;
 	}
 
-	ec = ParseFDSegList(&fd, dd_tot, path, bps);
+	ec = ParseFDSegList_simple(&fd, dd_tot, path, bps);
 	if (ec != EFD_OK)
 	{
 		return ec;
 	}
 
 	char out_file[32];
-	int cx = snprintf(out_file, 32, "./lsn_%06x", *lsn);
+	int cx = snprintf(out_file, 32, "./lsn_%06x", lsn);
 	if (cx <= 0 || cx > 32)
 	{
 		fprintf(stderr, "unable to make file name\n");
 		exit(-1);
 	}
+
+	ec = MarkFileSectors(os9_path, &fd, secondaryBitmap, lsn);
 
 	ec = SaveFDToFile(os9_path, &fd, out_file, bps);
 	if (ec != EFD_OK)
@@ -180,5 +200,68 @@ static error_code ProcessFileDescriptor(os9_path_id os9_path, u_int *lsn, char *
 		exit(-1);
 	}
 
+	return ec;
+}
+
+static error_code MarkFileSectors(os9_path_id os9_path, fd_stats *fd, unsigned char* secondaryBitmap, u_int lsn)
+{
+	error_code	ec = 0;
+	u_int  		i = 0, j, once;
+	Fd_seg		theSeg;
+	u_int 		num, curLSN;
+	u_int		dd_tot = int3(os9_path->lsn0->dd_tot);
+
+	while( int3(fd->fd_seg[i].lsn) != 0 )
+	{
+		if( i > NUM_SEGS )
+		{
+			i++;
+			break;
+		}
+
+		theSeg = &(fd->fd_seg[i]);
+		num = int2(theSeg->num);
+
+		if( (int3(theSeg->lsn) + num) > dd_tot )
+		{
+			printf("*** Bad FD segment (0x%06x-0x%06x) for LSN: 0x%06x (Segment index: %d)\n", int3(theSeg->lsn), int3(theSeg->lsn)+num, lsn, i );
+			i++;
+			continue;
+		}
+
+		for(j = 0; j < num; j++)
+		{
+			once = 0;
+			curLSN = int3(theSeg->lsn)+j;
+			
+			/* check for segment elements out of bounds */
+			if( curLSN > dd_tot )
+			{
+				if( once == 0 )
+				{
+					printf("*** Bad FD segment (0x%06x-0x%06x) for LSN 0x%06x (Segment index: %d)\n", int3(theSeg->lsn), int3(theSeg->lsn)+num, lsn, i );
+					once = 1;
+					ec = 1;
+				}
+			}
+			else
+			{
+				/* Check if bit is already allocated */
+				if ( _os9_ckbit( secondaryBitmap, curLSN ) != 0 )
+				{
+					/* Whoops, it is already allocated! */
+					// printf("Sector 0x%06x was previously allocated\n", curLSN );
+				}
+				else
+				{
+					/* Allocate bit and move on */
+					_os9_allbit( secondaryBitmap, curLSN, 1);
+				}
+			}
+		}
+		
+		i++;
+	}
+	
 	return ec;
 }
